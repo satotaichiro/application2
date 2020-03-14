51994f229b4079d8955805dc18b976302b146ad3
satotaichiro <tapoya09@gmail.com>
2020-03-14 20:49:24 +0900

    finish[5-3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f05a3afd89cae416cb7abd307cb8d149749791224c9c8f83259f5614cba8ff77cb2316158185c6b7a9b566c96255361b31142be9168201d73e5815cc132ce6ba'
